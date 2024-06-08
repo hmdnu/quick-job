2b6ccd32533c58e8b36e38f649303f6f29e8c96d
Lavina-23 <lavinakohai3227@gmail.com>
2024-06-08 23:02:10 +0700

    feat: create navbar include responsivity

--- client/src/constant/index.ts
+++ client/src/constant/index.ts
@@ -1,3 +1,13 @@
 /* 
     this folder is used to save your constant not changing variable
 */
+export const PROFILE_LINKS = [
+  { href: "/", key: "riwayat", icon: "/img/time.svg", label: "Riwayat" },
+  {
+    href: "/",
+    key: "pengaturan",
+    icon: "/img/setting.svg",
+    label: "Pengaturan",
+  },
+  { href: "/", key: "log-out", icon: "/img/log-out.svg", label: "Log Out" },
+];
