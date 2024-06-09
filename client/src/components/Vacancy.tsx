8f13a0bc71f692168bbf18dcaf6421a7664ce097
Lavina-23 <lavinakohai3227@gmail.com>
2024-06-09 21:55:44 +0700

    feat: create vacancy card include responsivity

---
+++ client/src/components/Vacancy.tsx
@@ -0,0 +1,166 @@
+const Vacancy = () => {
+  return (
+    <section className="md:flex md:justify-center lg:justify-start h-full xl:ml-[120px] mx-[20px] my-[20px] gap-[20px]">
+      <div className="grid md:inline-block gap-[20px]">
+        <div className="md:mb-[20px] max-w-sm p-4 gap-[10px] bg-white border border-gray-200 rounded-lg shadow">
+          <div className="flex justify-between">
+            <div className="flex flexCenter gap-[10px]">
+              <img
+                src="/img/user2.jpg"
+                alt="user"
+                className="w-[40px] h-[40px] lg:w-[50px] lg:h-[50px] rounded-full"
+              />
+              <div className="flex lg:grid gap-1 items-center justify-start">
+                <h1 className="text-bulma text-sm-s">Camilla</h1>
+                <div className="lg:hidden bg-bulma h-1 w-1 rounded-full"></div>
+                <h6 className="text-trunks text-xs-r">1 menit lalu</h6>
+              </div>
+            </div>
+            <div className="lg:hidden lg:mt-[20px] h-full inline-block rounded-lg bg-chici-90 bg-opacity-30 px-[6px] pt-[6px]">
+              <h5 className="mb-2 text-sm-s text-chici-90">30 menit</h5>
+            </div>
+            <div className="hidden lg:flex">
+              <h1 className="text-lg-s">Rp 50.000</h1>
+            </div>
+          </div>
+          <div className="hidden mt-[20px] lg:inline-block rounded-lg bg-chici-90 bg-opacity-30 px-[6px] pt-[6px]">
+            <h5 className="mb-2 text-sm-s text-chici-90">
+              Kadaluarsa dalam 30 menit
+            </h5>
+          </div>
+          <div className="grid mt-[10px] gap-[10px] md:gap-0">
+            <h1 className="text-sm-s md:text-md-s text-bulma">
+              Tolong dong yg bisa jastip jajan
+            </h1>
+            <p className="line-clamp-1 lg:line-clamp-none text-xs-r md:text-sm-r text-trunks">
+              Titip belikan makaroni ngehe hehehe, kirim ke gedung AX Polinema
+              ya !
+            </p>
+            <span className="text-xs-r md:text-sm-r text-trunks">
+              Lowokwaru, Malang
+            </span>
+          </div>
+          <div className="mt-[10px] flex justify-end gap-[5px]">
+            <button className="btn-sm-fill rounded-full bg-orange-90 text-white hover:text-orange-90 focus:text-orange-90 text-sm font-semibold">
+              <span className="hidden lg:inline">Hubungi Klien</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="lg:hidden size-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 0 1-2.555-.337A5.972 5.972 0 0 1 5.41 20.97a5.969 5.969 0 0 1-.474-.065 4.48 4.48 0 0 0 .978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25Z"
+                />
+              </svg>
+            </button>
+            <button className="btn-sm-fill bg-green-90 text-white hover:text-green-90 focus:text-green-90 text-sm font-semibold">
+              <span className="hidden lg:inline">Kerjakan</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="lg:hidden w-6 h-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M5 13l4 4L19 7"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+        <div className="md:mb-[20px] max-w-sm p-4 gap-[10px] bg-white border border-gray-200 rounded-lg shadow">
+          <div className="flex justify-between">
+            <div className="flex flexCenter gap-[10px]">
+              <img
+                src="/img/user2.jpg"
+                alt="user"
+                className="w-[40px] h-[40px] lg:w-[50px] lg:h-[50px] rounded-full"
+              />
+              <div className="flex lg:grid gap-1 items-center justify-start">
+                <h1 className="text-bulma text-sm-s">Camilla</h1>
+                <div className="lg:hidden bg-bulma h-1 w-1 rounded-full"></div>
+                <h6 className="text-trunks text-xs-r">1 menit lalu</h6>
+              </div>
+            </div>
+            <div className="lg:hidden lg:mt-[20px] h-full inline-block rounded-lg bg-chici-90 bg-opacity-30 px-[6px] pt-[6px]">
+              <h5 className="mb-2 text-sm-s text-chici-90">30 menit</h5>
+            </div>
+            <div className="hidden lg:flex">
+              <h1 className="text-lg-s">Rp 50.000</h1>
+            </div>
+          </div>
+          <div className="hidden mt-[20px] lg:inline-block rounded-lg bg-chici-90 bg-opacity-30 px-[6px] pt-[6px]">
+            <h5 className="mb-2 text-sm-s text-chici-90">
+              Kadaluarsa dalam 30 menit
+            </h5>
+          </div>
+          <div className="grid mt-[10px] gap-[10px] md:gap-0">
+            <h1 className="text-sm-s md:text-md-s text-bulma">
+              Tolong dong yg bisa jastip jajan
+            </h1>
+            <p className="line-clamp-1 lg:line-clamp-none text-xs-r md:text-sm-r text-trunks">
+              Titip belikan makaroni ngehe hehehe, kirim ke gedung AX Polinema
+              ya !
+            </p>
+            <span className="text-xs-r md:text-sm-r text-trunks">
+              Lowokwaru, Malang
+            </span>
+          </div>
+          <div className="mt-[10px] flex justify-end gap-[5px]">
+            <button className="btn-sm-fill rounded-full bg-orange-90 text-white hover:text-orange-90 focus:text-orange-90 text-sm font-semibold">
+              <span className="hidden lg:inline">Hubungi Klien</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="lg:hidden size-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 0 1-2.555-.337A5.972 5.972 0 0 1 5.41 20.97a5.969 5.969 0 0 1-.474-.065 4.48 4.48 0 0 0 .978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25Z"
+                />
+              </svg>
+            </button>
+            <button className="btn-sm-fill bg-green-90 text-white hover:text-green-90 focus:text-green-90 text-sm font-semibold">
+              <span className="hidden lg:inline">Kerjakan</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="lg:hidden w-6 h-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M5 13l4 4L19 7"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="md:basis-3/5 lg:basis-2/5 p-[30px] hidden h-[547px] md:grid justify-items-center border border-gray-200 rounded-lg shadow">
+        <h1 className="text-center text-green-90 text-2xl-s">
+          Tampilkan Detail Pekerjaan Disini
+        </h1>
+        <img src="/img/larry-home.svg" alt="larry-home" />
+      </div>
+    </section>
+  );
+};
+
+export default Vacancy;
