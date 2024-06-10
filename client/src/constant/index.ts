2e8cdb7427814358b14c3d176d7aaebd6792179f
Lavina-23 <lavinakohai3227@gmail.com>
2024-06-10 20:49:01 +0700

    feat: ui slicing for detail vacancy page

--- client/src/constant/index.ts
+++ client/src/constant/index.ts
@@ -11,3 +11,28 @@ export const PROFILE_LINKS = [
   },
   { href: "/", key: "log-out", icon: "/img/log-out.svg", label: "Log Out" },
 ];
+
+export const DUMMY_VACANCY = [
+  {
+    key: "0",
+    userImg: "/img/user.jpg",
+    username: "Lavina Kim",
+    timestamp: "1 menit",
+    sallary: "50.000",
+    exp: "30 menit",
+    title: "Tolong dong yg bisa jastip jajan",
+    desc: "Titip belikan makaroni ngehe hehehe, kirim ke gedung AX Polinema ya ! Rincian: Level 1 nya 2 Level 2 nya 1 Level 3 nya gak level :P Kalau kurang jelas bisa tanya duluu yaa lewat chatt Mwakasiii",
+    location: "Lowokwaru, Malang",
+  },
+  {
+    key: "1",
+    userImg: "/img/user2.jpg",
+    username: "Aqweena Kim",
+    timestamp: "2 menit",
+    sallary: "120.000",
+    exp: "15 menit",
+    title: "Ada yg bisa ngajarin materi gauss ?",
+    desc: "Butuh seseorang yang ngelesin gue matematika materi gauss...",
+    location: "Dinoyo, Malang",
+  },
+];
